fbd2c275c671f34a3ad4653e174570a8bdeb56d5
Luong Huy <luonghuy20032002@gmail.com>
2023-11-28 14:03:04 +0700

    Add Backend.

---
+++ Backend/Program.cs
@@ -0,0 +1,50 @@
+using mysignalR.Hubs;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+//th�m signalR
+builder.Services.AddSignalR();
+
+//th�m cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+                    .WithOrigins("http://localhost:3000"/*, "https://hoapham-chatapp.vercel.app"*/)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chatHub");
+});
+app.MapControllers();
+
+app.Run();
