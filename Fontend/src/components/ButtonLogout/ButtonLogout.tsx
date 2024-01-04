b345f42eda576d2405cf8994d81b0a06649144fc
KhoaHieu <khoahieu2982002@gmail.com>
2024-01-04 22:47:06 +0700

    update fontend

--- Fontend/src/components/ButtonLogout/ButtonLogout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { logoutUserWithSupabase } from "../../services/userAuthentication";
-import { Button } from "@chakra-ui/react";
-
-  
-export default ButtonLogout;
